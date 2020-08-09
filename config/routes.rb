20618ea6ef1bc8e16060d37361ad4d32bfcd1011
yuji.fujimoto <dir-zan@hotmail.co.jp>
2020-08-09 06:52:43 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   
+  # ログイン
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  # users
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
 end
